e8806fee588ec631ac89f72514355108b6959433
AAA <li2185@purdue.edu>
2020-04-12 02:59:10 -0400

    download done

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,26 +3,40 @@ const app = getApp()
 
 Page({
   data: {
-    cdlist: ""
+    cdlist: [],
+    testtext: "hello"
   },
 
-  onLoad: function() {
-    let retrived =[]
-    wx.cloud.callFunction({
-      name: 'getcdlist',
-      data: {
-      },
-      success: function(res) {
-        retrived.concat(res.result.data)
-        
-        console.log(res.result.data)
-      },
-      fail: console.error
-    })
+  onLoad: async function()  {
+    const db = wx.cloud.database()
+    let tasks = []
+    for (let i = 0; i < 5; i++) {
+      const promise = db.collection('cd').skip(i*20).limit(20).get()
+      tasks = tasks.concat(promise)
+    }
+    const f1 = (prelist) => {
+      let list = []
+      for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < prelist[i].data.length; j++) {
+          list.push(prelist[i].data[j])
+        }
+      }
+      return list
+    }
+    let x = await f1(await Promise.all(tasks))
+
+
+    let namelist = new Array
+    for (let i = 0; i < 95; i++) {
+      namelist.push(x[i].name)
+    }
+    console.log(x)
+    console.log(namelist)
     this.setData({
-      cdlist: retrived
+      cdlist: namelist
     })
-    console.log(retrived)
   },
+  
 
+  
 })
