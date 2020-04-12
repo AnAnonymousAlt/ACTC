ca4d6dd04cd15af9fdb9fbd53843ab51c7d7e55a
AAA <li2185@purdue.edu>
2020-04-11 20:08:17 -0400

    getcdlist

--- cloudfunctions/getcdlist/index.js
+++ cloudfunctions/getcdlist/index.js
@@ -1,12 +1,23 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-
 cloud.init()
-
+const db = cloud.database()
+const MAX_LIMIT = 100
 // 云函数入口函数
 exports.main = async (event, context) => {
-  
+  const countResult = await db.collection('cd').count()
+  const total = countResult.total
+  const batchTimes = Math.ceil(total / MAX_LIMIT)
 
-  return {
+  const tasks = []
+  for (let i = 0; i < batchTimes; i++) {
+    const promise = db.collection('cd').skip(i * MAX_LIMIT).limit(MAX_LIMIT).get()
+    tasks.push(promise)
   }
+  return( await Promise.all(tasks)).reduce((acc, cur) => {
+    return {
+      data: acc.data.concat(cur.data),
+      errMsg: acc.errMsg
+    }
+  })
 }
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,11 +3,26 @@ const app = getApp()
 
 Page({
   data: {
-    
+    cdlist: ""
   },
 
   onLoad: function() {
-   
+    let retrived =[]
+    wx.cloud.callFunction({
+      name: 'getcdlist',
+      data: {
+      },
+      success: function(res) {
+        retrived.concat(res.result.data)
+        
+        console.log(res.result.data)
+      },
+      fail: console.error
+    })
+    this.setData({
+      cdlist: retrived
+    })
+    console.log(retrived)
   },
 
 })
